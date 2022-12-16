a124c16db14e1a80adb3098a1326da2453ea7732
Xeradream <xeradream@gmail.com>
2022-12-16 11:08:36 +0100

    Q6

--- src/index.php
+++ src/index.php
@@ -5,11 +5,11 @@ namespace App;
 const __PROJECT_ROOT__  = __DIR__;
 
 use App\Controllers\Hotel\HotelListController;
-use App\Services\Hotel\UnoptimizedHotelService;
+use App\Services\Hotel\OneRequestHotelService;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$hotelService = UnoptimizedHotelService::getInstance();
+$hotelService = OneRequestHotelService::getInstance();
 
 $controller = new HotelListController( $hotelService );
 $controller->render();
